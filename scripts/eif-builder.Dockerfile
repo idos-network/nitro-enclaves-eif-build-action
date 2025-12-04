da33d78aff2b4f2d6f9e9f84016bd4737cf899be
Jan Strnádek <jan.strnadek@gmail.com>
2025-12-04 17:52:00 +0100

    Test build

--- scripts/eif-builder.Dockerfile
+++ scripts/eif-builder.Dockerfile
@@ -1,5 +1,4 @@
-# amazonlinux:2023.5.20240624.0 linux/amd64
-FROM amazonlinux@sha256:5bf791027b4659e73c33a88a3fa2b314b8e2c0ee60cb1088a097171ee7f180db
+FROM amazonlinux@2023
 
 # Install nitro-cli
 RUN dnf install aws-nitro-enclaves-cli aws-nitro-enclaves-cli-devel -y
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,7 @@ if [ -d "/custom-kernel-blobs" ] && [ "$(ls -A /custom-kernel-blobs)" ]; then
   echo "Custom kernel blobs found. Copying to /usr/share/nitro_enclaves/blobs..."
   rm -rf /usr/share/nitro_enclaves/blobs/
   cp -a /custom-kernel-blobs/ /usr/share/nitro_enclaves/blobs
+  ls -lh /usr/share/nitro_enclaves/blobs
 else
   echo "No custom kernel blobs provided."
 fi
